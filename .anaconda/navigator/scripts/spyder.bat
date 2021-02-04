7537c5037a9848923a6aa14563a9c76dc8352257
ali.hfph@gmail.com <ali.hfph@gmail.com>
2021-02-04 11:39:57 +0100

    this is console lo

---
+++ .anaconda/navigator/scripts/spyder.bat
@@ -0,0 +1,3 @@
+chcp 1252
+call C:\Users\Al-Gujjar\Anaconda3\Scripts\activate C:\Users\Al-Gujjar\Anaconda3
+spyder >C:\Users\Al-Gujjar\.anaconda\navigator\scripts\spyder-out-2.txt 2>C:\Users\Al-Gujjar\.anaconda\navigator\scripts\spyder-err-2.txt
