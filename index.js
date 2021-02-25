8bfbf1c4181a99438dfb085f09df9ddbd990db22
alihfph <ali.hfph@gmail.com>
2021-02-25 17:08:54 +0100

    This is homework

---
+++ index.js
@@ -0,0 +1,9 @@
+function deleterow() {
+    let getID = document.getElementById("delete")
+    getID.innerHTML = ""
+
+  }
+
+  function myFunction() {
+    setTimeout(function(){ alert("You have added the song"); }, 1000);
+  }
