ce58326c07eca78f2c3da18aff004c0e4f44956c
alihfph <ali.hfph@gmail.com>
2021-03-30 17:05:45 +0200

    hw

--- Desktop/Strive.school/M4/strivestaurant-feb21-D4-Thu/src/components/DishComments.jsx
+++ Desktop/Strive.school/M4/strivestaurant-feb21-D4-Thu/src/components/DishComments.jsx
@@ -1,16 +1,25 @@
 import { ListGroup } from 'react-bootstrap'
+import react from 'react'
 
-const DishComments = (props) =>
-    props.selectedDish
-        ? <ListGroup>
-            <h2>Comments for {props.selectedDish.name}</h2>
-            {
-                props.selectedDish.comments.map(c => (
-                    <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
-                ))
-            }
-        </ListGroup>
-        : <h1>NOTHING YET</h1>
+// const DishComments = (props) =>
+//     props.selectedDish
+//         ? <ListGroup>
+//             <h2>Comments for {props.selectedDish.name}</h2>
+//             {
+//                 props.selectedDish.comments.map(c => (
+//                     <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
+//                 ))
+//             }
+//         </ListGroup>
+//         : <h1>NOTHING YET</h1>
+  
+class DishComments extends React.Component {
+  
+    render() {
+      return <p>Hello </p> 
+    }
+    
+  }
 
 
 export default DishComments
